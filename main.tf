e58aefb453f179cc15760defe78f4c5bba7d0b58
Jared Wilson <jwilson@spear.ai>
2025-02-13 21:51:59 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,25 @@
 resource "proxmox_vm_qemu" "ubuntu" {
   count       = var.vm_count
   name        = "${var.vm_name}-${count.index + 1}"
-  target_node = "proxmox" # Change to your Proxmox node name
+  target_node = "proxmox"         # Change to your Proxmox node name
   clone       = "ubuntu-template" # Change to your template name
-  
-  cores       = var.vm_cores
-  memory      = var.vm_memory
+
+  cores  = var.vm_cores
+  memory = var.vm_memory
   disk {
     size    = var.vm_disk_size
     type    = "scsi"
     storage = "local-lvm"
   }
-  
+
   network {
     model  = "virtio"
     bridge = var.vm_network
   }
-  
+
   os_type = "cloud-init"
   sshkeys = "your-ssh-key-here"
-  
+
   lifecycle {
     ignore_changes = [network, sshkeys]
   }
