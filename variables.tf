9e2e3c54080c72e5bbb67189fce49afe2051ef7c
Jared Wilson <jwilson@spear.ai>
2025-02-13 21:53:41 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,21 +1,42 @@
-variable "proxmox_api_url" {}
-variable "proxmox_user" {}
-variable "proxmox_password" {}
+variable "proxmox_api_url" {
+  type = string
+}
+
+variable "proxmox_user" {
+  type = string
+}
+
+variable "proxmox_password" {
+  type      = string
+  sensitive = true
+}
+
 variable "vm_count" {
+  type    = number
   default = 2
 }
+
 variable "vm_name" {
+  type    = string
   default = "ubuntu-vm"
 }
+
 variable "vm_memory" {
+  type    = number
   default = 2048
 }
+
 variable "vm_cores" {
+  type    = number
   default = 2
 }
+
 variable "vm_disk_size" {
+  type    = string
   default = "10G"
 }
+
 variable "vm_network" {
+  type    = string
   default = "vmbr0"
 }
