7d650da55860aad413d69918fb993e4c14bd1fb7
Jared Wilson <jared.d.wilson24@gmail.com>
2025-02-17 21:23:52 -0500

    switch to api keys (#5)
    Co-authored-by: Jared Wilson <jwilson@spear.ai>

--- variables.tf
+++ variables.tf
@@ -2,11 +2,12 @@ variable "proxmox_api_url" {
   type = string
 }
 
-variable "proxmox_user" {
-  type = string
+variable "proxmox_api_token_id" {
+  type      = string
+  sensitive = true
 }
 
-variable "proxmox_password" {
+variable "proxmox_api_token_secret" {
   type      = string
   sensitive = true
 }
