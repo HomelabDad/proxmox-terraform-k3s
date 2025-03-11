b403c83bc793bd40cf8073fef1ce525937f06593
Jared Wilson <jwilson@spear.ai>
2025-03-10 21:13:26 -0400

    fmt
    fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,20 +2,20 @@
 resource "proxmox_vm_qemu" "k3s_control" {
   # VM name and basic configuration
   name        = "k3s-control"
-  target_node = "proxmox"  # The Proxmox host to deploy on
-  clone       = "k3s-control-template"  # Template to clone from
+  target_node = "proxmox"              # The Proxmox host to deploy on
+  clone       = "k3s-control-template" # Template to clone from
 
   # Hardware specifications
   cores  = 4
-  memory = 4096  # RAM in MB
-  scsihw = "virtio-scsi-pci"  # SCSI controller type
+  memory = 4096              # RAM in MB
+  scsihw = "virtio-scsi-pci" # SCSI controller type
 
   # Primary disk configuration
   disk {
     slot    = "scsi0"
     size    = "40G"
     type    = "disk"
-    storage = "local-lvm"  # Storage pool in Proxmox
+    storage = "local-lvm" # Storage pool in Proxmox
   }
   # Cloud-init disk for initialization
   disk {
@@ -28,18 +28,18 @@ resource "proxmox_vm_qemu" "k3s_control" {
   # Network configuration
   network {
     id     = 0
-    model  = "virtio"  # Network adapter type
-    bridge = "vmbr0"   # Bridge interface on Proxmox host
+    model  = "virtio" # Network adapter type
+    bridge = "vmbr0"  # Bridge interface on Proxmox host
   }
 
   # Cloud-init configuration
   os_type   = "cloud-init"
-  ipconfig0 = "ip=${var.control_node_ip}/24,gw=${var.vm_gateway}"  # Static IP configuration
-  sshkeys   = var.ssh_key  # SSH key for authentication
+  ipconfig0 = "ip=${var.control_node_ip}/24,gw=${var.vm_gateway}" # Static IP configuration
+  sshkeys   = var.ssh_key                                         # SSH key for authentication
 
-  ciuser       = "ubuntu"  # Default user
-  nameserver   = "8.8.8.8"  # DNS server
-  searchdomain = "vermillion.local"  # Search domain for DNS
+  ciuser       = "ubuntu"           # Default user
+  nameserver   = "8.8.8.8"          # DNS server
+  searchdomain = "vermillion.local" # Search domain for DNS
 
   automatic_reboot = true
 
@@ -54,9 +54,9 @@ resource "proxmox_vm_qemu" "k3s_control" {
 
     inline = [
       "curl -sfL https://get.k3s.io | sh -s - server",
-      "sleep 10",  # Give K3s time to initialize
-      "sudo cp /var/lib/rancher/k3s/server/node-token /home/ubuntu/node-token",    # Copy to a location we can access
-      "sudo chown ubuntu:ubuntu /home/ubuntu/node-token"                           # Change ownership to ubuntu user
+      "sleep 10",                                                               # Give K3s time to initialize
+      "sudo cp /var/lib/rancher/k3s/server/node-token /home/ubuntu/node-token", # Copy to a location we can access
+      "sudo chown ubuntu:ubuntu /home/ubuntu/node-token"                        # Change ownership to ubuntu user
     ]
   }
 
@@ -73,8 +73,8 @@ resource "proxmox_vm_qemu" "k3s_control" {
 
 # K3s Worker Nodes
 resource "proxmox_vm_qemu" "k3s_worker" {
-  count       = var.worker_count  # Number of worker nodes to create
-  name        = "k3s-worker-${count.index + 1}"  # Numbered name for each worker
+  count       = var.worker_count                # Number of worker nodes to create
+  name        = "k3s-worker-${count.index + 1}" # Numbered name for each worker
   target_node = "proxmox"
   clone       = "k3s-worker-template"
 
@@ -140,7 +140,7 @@ resource "proxmox_vm_qemu" "k3s_worker" {
     inline = [
       "TOKEN=$(cat /tmp/node-token)",
       "curl -sfL https://get.k3s.io | K3S_URL=https://${var.control_node_ip}:6443 K3S_TOKEN=\"$TOKEN\" sh -",
-      "rm /tmp/node-token"  # Clean up the token file
+      "rm /tmp/node-token" # Clean up the token file
     ]
   }
 
